1431313dd0138b8e0830b8b6bd2c6ab379b34e25
josefinaac95 <josefina.acevedo95@gmail.com>
2023-04-08 23:24:34 -0300

    Correcciones

---
+++ cypress/selectores/misselectores.js
@@ -0,0 +1,77 @@
+class pantallas{
+
+    login = {
+        "ingresoallogin": '#login2',
+        "corroborotextologin": '#logInModalLabel',
+        "name_login": '#loginusername',
+        "password_word": '#logInModal > div > div > div.modal-body > form > div:nth-child(2) > label',
+        "pass_login": '#loginpassword',
+        "recuadrodelogin": '#logInModal > div > div',
+        "welcome_name": '#nameofuser'
+    }
+
+    phones = {
+        "btn_phone": '#itemc',
+        "texto_nexus": '#tbodyid > div:nth-child(3) > div > div > h5',
+        "btn_nexus_6": '#tbodyid > div:nth-child(3) > div > div > h4 > a',
+        "btn_addtocard": '#tbodyid > div.row > div > a'
+    }
+
+    pagina_principal = {
+        "btn_home": '#navbarExample > ul > li.nav-item.active > a',
+        "texto_productstore": '#nava'
+    }
+
+    laptops = {
+        "btn_laptop": '#contcont > div > div.col-lg-3 > div',
+        "texto_sony": '#tbodyid > div:nth-child(1) > div > div > h4 > a',
+        "btn_sony_i7": '#contcont > div > div.col-lg-9'
+    }
+
+    monitors = {
+        "btn_monitor": '#contcont > div > div.col-lg-3 > div',
+        "texto_asus": '#tbodyid > div:nth-child(2) > div > div > h4 > a',
+        "btn_monitor_aple": '#tbodyid > div:nth-child(1) > div > div > h4 > a'
+    }
+
+    carrito_de_compras = {
+        "btn_carrito": '#cartur',
+        "btn_placeorder": '#page-wrapper > div > div.col-lg-1 > button',
+        "texto_total": '#totalm',
+        "name_formulario": '#name',
+        "country_formulario": '#country',
+        "city_formulario": '#city',
+        "card_formulario": '#card',
+        "month_formulario": '#month',
+        "year_formulario": '#year',
+        "formulario": '#orderModal > div > div > div.modal-body > form',
+        "btn_purcharse": '#orderModal > div > div > div.modal-footer > button.btn.btn-primary',
+        "alertadecompraexitosa": 'body > div.sweet-alert.showSweetAlert.visible',
+        "btn_ok": 'body > div.sweet-alert.showSweetAlert.visible > div.sa-button-container > div > button'
+    }
+
+    desloguearse = {
+        "btn_desloguearse": '#logout2'
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = new pantallas();
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,78 +1,92 @@
+import pantallas from '../selectores/misselectores'
+
 //Este comando es para conectarme a la web
 Cypress.Commands.add('conectarmealaweb', () => {
     cy.visit('https://www.demoblaze.com/')
 })
 
+//Este comando es para escribir en un cuadro de texto
+Cypress.Commands.add('escribir', (cuadro_de_texto, texto) => {
+    cy.get(cuadro_de_texto).type(texto)
+})
+
+//Este comando es para clickear
+Cypress.Commands.add('mi_click', (cuadro_de_texto) =>{
+    cy.get(cuadro_de_texto).click()
+})
+
 //Este comando es para ingresar al LOG IN
 Cypress.Commands.add('ingresoallogin', () =>{
     cy.screenshot()
-    cy.get('#login2').click()
-    cy.get('#logInModalLabel').should('contain','Log in')
+    cy.mi_click(pantallas.login.ingresoallogin)
+    cy.get(pantallas.login.corroborotextologin).should('contain','Log in')
     cy.wait(1000)
 })
 
 //Este comando es para realizar el LOG IN
 Cypress.Commands.add('realizarlogin', (log_in)=>{
-    cy.get('#loginusername').type('Josefinaac95').then((result) => {
+    cy.fixture('login').then(datoslogin => {
+    cy.escribir(pantallas.login.name_login, datoslogin.name_login).then((result) => {
         const { value } = result[0];
         expect(value).to.equal('Josefinaac95');
     });
-    cy.get('#logInModal > div > div > div.modal-body > form > div:nth-child(2) > label').should('contain', 'Password')
-    cy.get('#loginpassword').type('Prueba1234').then((result) => {
+    cy.get(pantallas.login.password_word).should('contain', 'Password')
+    cy.escribir(pantallas.login.pass_login, datoslogin.pass_login).then((result) => {
         const { value } = result[0];
         expect(value).to.equal('Prueba1234');
     });
-    cy.get('#logInModal > div > div').screenshot()
-    cy.get('#logInModal > div > div').find('button').eq(2).click()
+    cy.get(pantallas.login.recuadrodelogin).screenshot()
+    cy.get(pantallas.login.recuadrodelogin).find('button').eq(2).click()
+})
 })
 
 //Este comando es para corroborar el LOG IN
 Cypress.Commands.add('corroborologin', () => {
-    cy.get('#nameofuser').should('contain','Welcome')
-    cy.get('#nameofuser').screenshot()
+    cy.get(pantallas.login.welcome_name).should('contain','Welcome')
+    cy.get(pantallas.login.welcome_name).screenshot()
 })
 
 //Este comando es para añadir un celular de la CATEGORIA celulares
 Cypress.Commands.add('añadocelular', () => {
-    cy.get('#itemc').click()
-    cy.get('#tbodyid > div:nth-child(3) > div > div > h5').should('contain', '650')
-    cy.get('#tbodyid > div:nth-child(3) > div > div > h4 > a').click()
+    cy.mi_click(pantallas.phones.btn_phone)
+    cy.get(pantallas.phones.texto_nexus).should('contain', '650')
+    cy.mi_click(pantallas.phones.btn_nexus_6)
     cy.wait(1000)
-    cy.get('#tbodyid > div.row > div > a').click()
+    cy.mi_click(pantallas.phones.btn_addtocard)
     cy.screenshot()
     cy.wait(1000)
 })
 
 //Este comando es para volver a la pagina principal y corroborar que estoy en ella
 Cypress.Commands.add('paginaprincipal', () =>{
-    cy.get('#navbarExample > ul > li.nav-item.active > a').click()
-    cy.get('#nava').should('contain', 'PRODUCT STORE')
+    cy.mi_click(pantallas.pagina_principal.btn_home)
+    cy.get(pantallas.pagina_principal.texto_productstore).should('contain', 'PRODUCT STORE')
     cy.wait(1000)
 })
 
 //Este comando es para añadir una computadora de la CATEGORIA computadoras
 Cypress.Commands.add('añadocomputadora', () => {
-    cy.get('#contcont > div > div.col-lg-3 > div').find('a').eq(2).click()
+    cy.get(pantallas.laptops.btn_laptop).find('a').eq(2).click()
     cy.wait(1000)
-    cy.get('#tbodyid > div:nth-child(1) > div > div > h4 > a').should('contain', 'i5')
-    cy.get('#contcont > div > div.col-lg-9').find('a').eq(2).click()
-    cy.get('#tbodyid > div.row > div > a').click()
+    cy.get(pantallas.laptops.texto_sony).should('contain', 'i5')
+    cy.get(pantallas.laptops.btn_sony_i7).find('a').eq(2).click()
+    cy.mi_click(pantallas.phones.btn_addtocard)
     cy.screenshot()
 })
 
 //Este comando es para añadir un monitor de la CATEGORIA monitores
 Cypress.Commands.add('añadomonitor', () => {
-    cy.get('#contcont > div > div.col-lg-3 > div').find('a').eq(3).click()
+    cy.get(pantallas.monitors.btn_monitor).find('a').eq(3).click()
     cy.wait(1000)
-    cy.get('#tbodyid > div:nth-child(2) > div > div > h4 > a').should('contain','ASUS')
-    cy.get('#tbodyid > div:nth-child(1) > div > div > h4 > a').click()
-    cy.get('#tbodyid > div.row > div > a').click()
+    cy.get(pantallas.monitors.texto_asus).should('contain','ASUS')
+    cy.mi_click(pantallas.monitors.btn_monitor_aple)
+    cy.mi_click(pantallas.phones.btn_addtocard)
     cy.screenshot()
 })
 
 //Este comando es para ir al carrito de compras y corroborar que me encuentro en el
 Cypress.Commands.add('carritodecompras', () =>{
-    cy.get('#cartur').click()
+    cy.mi_click(pantallas.carrito_de_compras.btn_carrito)
     cy.get('#page-wrapper > div > div.col-lg-8 > h2').should('contain', 'Products')
     cy.wait(1000)
     cy.screenshot()
@@ -80,43 +94,43 @@ Cypress.Commands.add('carritodecompras', () =>{
 
 //Este comando es para conectarme con el archivo json y rellenar el formulario de compras
 Cypress.Commands.add('formulariocompra', () =>{
-        cy.fixture('example').then(misdatos => {
-        cy.get('#page-wrapper > div > div.col-lg-1 > button').click()
-        cy.get('#totalm').should('contain', 'Total')
+        cy.fixture('formulario').then(misdatos => {
+        cy.mi_click(pantallas.carrito_de_compras.btn_placeorder)
+        cy.get(pantallas.carrito_de_compras.texto_total).should('contain', 'Total')
         cy.wait(2000)
-        cy.get('#name').type(misdatos.name).then((result) => {
+        cy.escribir(pantallas.carrito_de_compras.name_formulario, misdatos.name).then((result) => {
             const { value } = result[0];
             expect(value).to.equal('Josefina');
         });
-        cy.get('#country').type(misdatos.country).then((result) => {
+        cy.escribir(pantallas.carrito_de_compras.country_formulario, misdatos.country).then((result) => {
             const { value } = result[0];
             expect(value).to.equal('Argentina');
         });
-        cy.get('#city').type(misdatos.city).then((result) => {
+        cy.escribir(pantallas.carrito_de_compras.city_formulario, misdatos.city).then((result) => {
             const { value } = result[0];
             expect(value).to.equal('Buenos Aires');
         });
-        cy.get('#card').type(misdatos.card).then((result) => {
+        cy.escribir(pantallas.carrito_de_compras.card_formulario, misdatos.card).then((result) => {
             const { value } = result[0];
             expect(value).to.equal('08127776523');
         });
-        cy.get('#month').type(misdatos.month).then((result) => {
+        cy.escribir(pantallas.carrito_de_compras.month_formulario, misdatos.month).then((result) => {
             const { value } = result[0];
             expect(value).to.equal('Noviembre');
         });
-        cy.get('#year').type(misdatos.year).then((result) => {
+        cy.escribir(pantallas.carrito_de_compras.year_formulario, misdatos.year).then((result) => {
             const { value } = result[0];
             expect(value).to.equal('2025');
         });
-        cy.get('#orderModal > div > div > div.modal-body > form').screenshot()
-        cy.get('#orderModal > div > div > div.modal-footer > button.btn.btn-primary').click()
-        cy.get('body > div.sweet-alert.showSweetAlert.visible').screenshot()
-        cy.get('body > div.sweet-alert.showSweetAlert.visible > div.sa-button-container > div > button').click()
+        cy.get(pantallas.carrito_de_compras.formulario).screenshot()
+        cy.mi_click(pantallas.carrito_de_compras.btn_purcharse)
+        cy.get(pantallas.carrito_de_compras.alertadecompraexitosa).screenshot()
+        cy.mi_click(pantallas.carrito_de_compras.btn_ok)
     })
 })
 
 //Este comando es para desloguearse
 Cypress.Commands.add('desloguearse', () => {
-    cy.get('#logout2').click()
+    cy.mi_click(pantallas.desloguearse.btn_desloguearse)
     cy.screenshot()
 })
